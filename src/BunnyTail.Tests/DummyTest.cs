896d2d82e9a9480488d2325678546ff165a96492
Arialdo Martini <arialdomartini@gmail.com>
2013-03-05 17:04:51 +0100

    An empty project, with a dummy test project, and a build script able to download deps with NuGet, compile and run tests

---
+++ src/BunnyTail.Tests/DummyTest.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace Booz.Tests
+{
+	[TestFixture]
+	public class DummyTest
+	{
+		[Test]
+		public void Should_pass()
+		{
+			Assert.Pass();
+		}
+
+	}
+}
---
+++ src/BunnyTail/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Booz
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
